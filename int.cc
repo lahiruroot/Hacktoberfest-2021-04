6404c930a31b6633ab3fcfa373943fc5e8e0f991
mihinN <anonnimnaka@gmail.com>
2021-10-12 06:01:09 +0530

    hacktober in c++ ;))

--- int.cc
+++ int.cc
@@ -1,24 +1,28 @@
-public class MyClass
-{
-    public string  myField = string.Empty;
 
-    public MyClass()
-    {
-    }
+#include <iostream>
+#include <stdlib.h>
+//#include <windows.h>
+#include <string>
+using namespace std;
+class hackTober{
+      public:
+	      void octoberHack(string get_data){
+		      if(get_data =="yes"){
+		      cout <<"Cool XD"<<endl;
+		      }else if(get_data =="no"){
+		      cout << "*[ Hacktoberfest encourages participation in the open source community,\n which grows bigger every year.\n Complete the 2021 challenge and earn a limited edition T-shirt."<< endl;}else{
+			      cout << "just enter yes or no" << endl;
+		      }		
+	      }
+};
+int main(int argc,char*argv[]){
+  cout << "\n______________________________" << endl;
+  cout << "HacktoberFest User registration" << endl;
+  cout << "--------------------------------\n" << endl;
+  cout << "Need to get Details about HacktoberFest ? --(if you want type yes and dont want type no)" << endl;
+  string input_one;
+  cin >> input_one;
+  hackTober first_user;
+  first_user.octoberHack(input_one);
 
-    public void MyMethod(int parameter1, string parameter2)
-    {
-        Console.WriteLine("First Parameter {0}, second parameter {1}", 
-                                                    parameter1, parameter2);
-    }
-
-    public int MyAutoImplementedProperty { get; set; }
-
-    private int myPropertyVar;
-    
-    public int MyProperty
-    {
-        get { return myPropertyVar; }
-        set { myPropertyVar = value; }
-    } 
 }
